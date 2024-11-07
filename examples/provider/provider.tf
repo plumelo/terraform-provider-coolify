ef13b3b36cb13394f4155340903287fe58167fcc
SierraJC <7351311+SierraJC@users.noreply.github.com>
2024-11-07 12:31:02 +1100

    docs: update provider example

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -15,4 +15,24 @@ provider "coolify" {
   token = "Your API token"
 }
 
-# ...
+# Generate a new private key, and create a server with that key.
+
+resource "tls_private_key" "example" {
+  algorithm = "ED25519"
+}
+
+resource "coolify_private_key" "example" {
+  name        = "Example Terraformed Key"
+  description = "Managed by Terraform"
+  private_key = tls_private_key.example.private_key_pem
+}
+
+resource "coolify_server" "example" {
+  name             = "Example Terraformed Server"
+  description      = "Managed by Terraform"
+  ip               = "localhost"
+  port             = 22
+  user             = "root"
+  private_key_uuid = coolify_private_key.example.uuid
+  instant_validate = false
+}
