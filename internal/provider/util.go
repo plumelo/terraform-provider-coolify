568cfef2a1093adbc7d136ceec86fdf047efa3e6
SierraJC <7351311+SierraJC@users.noreply.github.com>
2024-11-07 22:02:49 +1100

    chore: add `optionalStringListValue` helper

--- internal/provider/util.go
+++ internal/provider/util.go
@@ -1,7 +1,9 @@
 package provider
 
 import (
+	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 )
 
 func optionalString(value *string) types.String {
@@ -23,4 +25,18 @@ func optionalBool(value *bool) types.Bool {
 		return types.BoolNull()
 	}
 	return types.BoolValue(*value)
+}
+
+// optionalStringListValue converts a list of strings to a ListValue.
+func optionalStringListValue(values *[]string) basetypes.ListValue {
+	if values == nil {
+		return types.ListNull(types.StringType)
+	}
+
+	elems := make([]attr.Value, len(*values))
+	for i, v := range *values {
+		elems[i] = types.StringValue(v)
+	}
+
+	return types.ListValueMust(types.StringType, elems)
 }
--- internal/provider/util_test.go
+++ internal/provider/util_test.go
@@ -3,6 +3,7 @@ package provider
 import (
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/stretchr/testify/assert"
 )
@@ -23,4 +24,38 @@ func TestOptionalBool(t *testing.T) {
 	input := true
 	assert.Equal(t, types.BoolNull(), optionalBool(nil))
 	assert.Equal(t, types.BoolValue(input), optionalBool(&input))
+}
+
+func TestOptionalStringListValue(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    *[]string
+		expected types.List
+	}{
+		{
+			name:     "nil input",
+			input:    nil,
+			expected: types.ListNull(types.StringType),
+		},
+		{
+			name:     "empty array input",
+			input:    &[]string{},
+			expected: types.ListValueMust(types.StringType, []attr.Value{}),
+		},
+		{
+			name:  "populated array input",
+			input: &[]string{"one", "two", "three"},
+			expected: types.ListValueMust(types.StringType, []attr.Value{
+				types.StringValue("one"),
+				types.StringValue("two"),
+				types.StringValue("three"),
+			}),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.expected, optionalStringListValue(tt.input))
+		})
+	}
 }
